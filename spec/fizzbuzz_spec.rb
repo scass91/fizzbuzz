1d9439beeb66cd02f1b12fe63332769d1d16de83
Simon Cass <scass1991@gmail.com>
2018-11-15 16:10:08 +0000

    fixed logic order for 15 divisor, added 2 further tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,10 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns 11 when passed 11' do
+    expect(fizzbuzz(11)).to eq 11
+  end
 end
